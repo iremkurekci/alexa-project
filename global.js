c729b7121149eb306cd0a2b7e72dbcb5815cca5e
İrem Kürekçi <ismihan.irem.1999@gmail.com>
2021-12-29 17:46:52 +0300

    committed
    committed

--- global.js
+++ global.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const Json2csvParser = require('json2csv').Parser;
+var path = require('path')
 
 const BASE_URL = 'https://www.alexa.com/topsites';
 
@@ -15,47 +18,104 @@ const global = {
 
         page = await browser.newPage();
 
+        await page.setDefaultNavigationTimeout(0); 
+
         await page.goto(BASE_URL, { waitUntil: 'networkidle2' });
 
     },
 
     count: async () => {
 
-        let lineCount = await page.evaluate(() => {
-            window.scrollBy(0, window.innerHeight)
-        })
+        try {
 
-        return (await page.$$('div[class="tr site-listing"]')).length;
+            let lineCount = await page.evaluate(() => {
+                window.scrollBy(0, window.innerHeight)
+            })
+
+            return (await page.$$('div[class="tr site-listing"]')).length;
+
+        } catch (error) {
+            console.log(error)
+        }
     },
-
 
     getDetails: async (lineBaseSelector) => {
+        try {
 
-        let url = await page.url();
+            let url = await page.url();
 
-        await page.waitForSelector(lineBaseSelector);
+            const details = await page.evaluate((x) => {
+                const data = {
+                    rank: document.querySelector(`${x} > div:nth-child(1)`).innerText,
+                    name: document.querySelector(`${x} > div.td.DescriptionCell > p > a`).innerText.toLowerCase(),
+                    url: document.querySelector(`${x} > div.td.DescriptionCell > p > a`).getAttribute('href'),
+                    last_update: Date.now()
+                }
+                return Promise.resolve(data);
+            }, lineBaseSelector);
 
-        let details = await page.evaluate(() => {
-            return {
-                rank: document.querySelector('div[class="td"]').innerText,
-                name: document.querySelector('p > a').innerText,
-                url: document.querySelector('p > a').getAttribute('href')
-            }
-        });
+            return details;
 
-        return details;
-
+        } catch (error) {
+            console.log(error)
+        }
     },
 
-    // #alx-content > div.row-fluid.TopSites.AlexaTool.padding20 > section.page-product-content.summary.padding20 > span > span > div > div > div > div:nth-child(2)
-    // #alx-content > div.row-fluid.TopSites.AlexaTool.padding20 > section.page-product-content.summary.padding20 > span > span > div > div > div > div:nth-child(2) > div:nth-child(1)
-    // #alx-content > div.row-fluid.TopSites.AlexaTool.padding20 > section.page-product-content.summary.padding20 > span > span > div > div > div > div:nth-child(2) > div.td.DescriptionCell > p > a
+    jsToJSON: async (data) => {
+
+        try {
+
+            let url = await page.url();
+
+            const cc = await page.evaluate(() => {
+                return {
+                    cc: document.querySelector('#alx-content > div.row-fluid.TopSites.AlexaTool.padding20 > section.page-product-top > div > section > div.Foldering > div.each.current > a').innerText.toLowerCase()
+                }
+            });
+
+            var _path = path.join(__dirname, `./alexadata/${cc.cc}`);
+
+            if (!fs.existsSync("alexadata")) {
+
+                fs.mkdir("./alexadata", (err) => {
+                    fs.mkdir(`./alexadata/${cc.cc}`, (err) => {
+                        if (err) return err;
+
+                        fs.writeFileSync(path.join(_path, './sites.json'), JSON.stringify(data), 'utf-8', (err) => {
+                            if (err) return err;
+
+                        });
+
+                    });
+                });
+            } else {
+
+                fs.mkdir(`./alexadata/${cc.cc}`, (err) => {
+                    if (err) return err;
+
+                    fs.writeFileSync(path.join(_path, './sites.json'), JSON.stringify(data), 'utf-8', (err) => {
+                        if (err) return err;
+
+                    });
+                });
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    timeToWaitRandomly: async () => {
+        let milisec = Math.floor(Math.random() * 3500) + 1000;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        page.waitForTimeout(milisec);
+    },
 
     end: async () => {
         await browser.close();
     }
 
 };
-
 
 module.exports = global;
--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const puppeteer = require('puppeteer');
 const global = require('./global');
-// const Json2csvParser = require('json2csv').Parser;
-// const fs = require('fs');
+const country = require('./country');
 
 try {
     (async () => {
 
         await global.initialize();
 
-        let lineCount = await global.count();
+        let itemCount_Global = await global.count();
         let globalData = [];
-        let childNum = null;
-        let lineBaseSelector = null;
-         
+        let childNum_Global = null;
+        let lineBaseSelector_Global = null;
 
-        for(let i = 1; i <= lineCount; i++)
-        {
-            childNum = i + 1;
-            lineBaseSelector = '#alx-content > div.row-fluid.TopSites.AlexaTool.padding20 > section.page-product-content.summary.padding20 > span > span > div > div > div > div:nth-child(' + `${childNum}` + ')';
-            let details = await global.getDetails(lineBaseSelector);
-            globalData.push(details);
+
+        for (let i = 1; i <= itemCount_Global; i++) {
+            childNum_Global = i + 1;
+            lineBaseSelector_Global = '#alx-content > div.row-fluid.TopSites.AlexaTool.padding20 > section.page-product-content.summary.padding20 > span > span > div > div > div > div:nth-child(' + `${childNum_Global}` + ')';
+            let detailsGlobal = await global.getDetails(lineBaseSelector_Global);
+            globalData.push(detailsGlobal);
         }
+
+        global.jsToJSON(globalData);
+
+        const puppeteer = require('puppeteer');
+
+        await global.timeToWaitRandomly();
+
+        await country.initialize();
+
+        let itemCount_Country = await country.count(); // [li,ul]
+        let countryData = [];
+        let ul_ChildNum_Country = null;
+        let li_ChildNum_Country = null;
+        let lineBaseSelector_Country = null;
+
+        for (let j = 1; j <= itemCount_Country[1]; j++) {
+            for (let i = 1; i <= itemCount_Country[0]; i++) {
+
+                li_ChildNum_Country = i;
+                ul_ChildNum_Country = j;
+                lineBaseSelector_Country = '#alx-content > div.row-fluid.TopSites.Alexarest > section.page-product-content.summary > span > span > div > div > div > div:nth-child(1) > ul:nth-child(' + `${ul_ChildNum_Country}` + ') > li:nth-child(' + `${li_ChildNum_Country}` + ') > a';
+                let detailsCountry = await country.getDetails(lineBaseSelector_Country);
+                countryData.push(detailsCountry);
+            }
+        }
+
+        await global.timeToWaitRandomly();
+        await country.getContentCountry(countryData);
+        // await countryData.map(item => country.getContentCountry(item));
 
         debugger;
 
